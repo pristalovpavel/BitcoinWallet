b31a4b025291e3bbbb978be4150e9bcbd5101991
Pristalov Pavel <p.pristalov@arammeem.com>
2026-03-14 18:32:39 +0200

    Remove Hilt plugin, clean up unused imports.

--- app/src/main/java/com/pristalovpavel/bitcoinwallet/ui/BitcoinWalletApp.kt
+++ app/src/main/java/com/pristalovpavel/bitcoinwallet/ui/BitcoinWalletApp.kt
@@ -17,9 +17,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.navigation.compose.NavHost
@@ -27,7 +25,6 @@ import androidx.navigation.compose.composable
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import com.pristalovpavel.bitcoinwallet.ui.theme.BitcoinWalletTheme
-import com.pristalovpavel.bitcoinwallet.utils.readDataFromFile
 import com.pristalovpavel.bitcoinwallet.viewmodel.BitcoinViewModel
 
 @OptIn(ExperimentalMaterial3Api::class)
